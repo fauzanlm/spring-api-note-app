6d482249aa85e18415abf1ee890d858e011ff671
Muhammad Fauzan <fauzan14072004@gmail.com>
2024-05-09 19:45:17 +0700

    add swagger ui

--- src/main/java/com/fauzanlm/apinoteapp/services/impl/NoteServiceImpl.java
+++ src/main/java/com/fauzanlm/apinoteapp/services/impl/NoteServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.fauzanlm.apinoteapp.model.Note;
@@ -13,32 +12,35 @@ import com.fauzanlm.apinoteapp.repository.NoteRepository;
 import com.fauzanlm.apinoteapp.services.NoteService;
 
 @Service
-public class NoteServiceImpl implements NoteService{
+public class NoteServiceImpl implements NoteService {
 	@Autowired
 	private NoteRepository repo;
-	
+
 	private ModelMapper mapper = new ModelMapper();
-	
+
 	@Override
 	public List<Note> getAll() {
 		// TODO Auto-generated method stub
 		return repo.findAll();
 	}
+
 	@Override
 	public Note createNote(NoteDTO data) {
 		// TODO Auto-generated method stub
 		Note dataResult = mapper.map(data, Note.class);
 		return repo.save(dataResult);
 	}
+
 	@Override
 	public Note updateNote(NoteDTO data, Integer id) {
 		// TODO Auto-generated method stub
-//		System.out.println(id + data.toString());
+		// System.out.println(id + data.toString());
 		Note dbRow = repo.findById(id).get();
 		Note dataResult = mapper.map(data, dbRow.getClass());
 		dataResult.setId(id);
 		return repo.save(dataResult);
 	}
+
 	@Override
 	public void deleteNote(Integer id) {
 		// TODO Auto-generated method stub
